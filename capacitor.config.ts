089964a56ae173cb0362afcf904a9cdb0ee8cbf3
Jacob Bishop-Townsend <jacobbishop-townsend@MacBook-Air.local>
2023-03-06 21:23:25 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.list',
+  appName: 'list',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
